bd15960d7ad43f7a5984cbc984df18f8987850aa
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-20 09:25:52 -0300

    feat: create database module

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { UserRepository } from 'src/modules/user/repositories/UserRepository';
+import { PrismaUserRepository } from './prisma/repositories/prismaUserRepository';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
+    },
+  ],
+  controllers: [],
+  exports: [UserRepository],
+})
+export class DatabaseModule {}
