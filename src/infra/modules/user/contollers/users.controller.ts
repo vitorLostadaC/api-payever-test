0f5c06d5f318efa1002bd03c364fbad19d46d221
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-20 09:43:04 -0300

    feat: create users controller

---
+++ src/infra/modules/user/contollers/users.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserUseCase } from '../../../../modules/user/useCases/createUserUseCase/createUserUseCase';
+import { CreateUserBody } from '../dtos/createUserBody';
+
+@Controller('users')
+export class UsersController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  @Post()
+  async create(@Body() body: CreateUserBody) {
+    const { first_name, last_name, name } = body;
+
+    const user = await this.createUserUseCase.execute({
+      first_name,
+      last_name,
+      name,
+    });
+
+    return user;
+  }
+}
