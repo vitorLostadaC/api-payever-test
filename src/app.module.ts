e786e105b4d1525f7aae62bec6c2d50c6dab602d
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-20 08:36:05 -0300

    feat: config env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
