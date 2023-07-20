2134bd9cf8ecbfed068f67d38758cb414784a811
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-20 09:47:46 -0300

    feat: add modules in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infra/database/database.module';
+import { UserModule } from './infra/modules/user/user.module';
 
 @Module({
   imports: [
+    DatabaseModule,
+    UserModule,
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
