2ce1e69f321fe42b449bf91fed92ab2bd3839704
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-20 16:30:55 -0300

    feat: change default class-validator error

--- src/infra/modules/user/dtos/createUserBody.ts
+++ src/infra/modules/user/dtos/createUserBody.ts
@@ -1,5 +1,14 @@
+import { IsEmailCustom } from 'src/infra/classValidator/decorators/isEmailCustom';
+import { IsNotEmptyCustom } from 'src/infra/classValidator/decorators/isNotEmptyCustom';
+
 export class CreateUserBody {
+  @IsNotEmptyCustom()
+  @IsEmailCustom()
   email: string;
+
+  @IsNotEmptyCustom()
   first_name: string;
+
+  @IsNotEmptyCustom()
   last_name: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationError, ValidationPipe } from '@nestjs/common';
+import { IncorrectValuesException } from './exception/incorrectValuesException';
+import { mapperClassValidateErrorToAppError } from './utils/mappers';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        throw new IncorrectValuesException(
+          mapperClassValidateErrorToAppError(errors),
+        );
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
